a7cfeac9dd3b3b1239171f5d0db2d4fb2984cfa8
Dinh Hoang <htdinh13@gmail.com>
2021-05-05 08:24:53 +1200

    Initialize project

---
+++ recuit-ios-DannyHoang/recuit-ios-DannyHoang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  recuit-ios-DannyHoang
+//
+//  Created by Truong Dinh Hoang on 5/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
